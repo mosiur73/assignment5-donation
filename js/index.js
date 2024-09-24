eeb11a7952a352cac229c121aa204228e78987f5
mosiur49 <bpi2021cmt44@gmail.com>
2024-09-24 12:43:43 +0600

    navbar complate

---
+++ js/index.js
@@ -0,0 +1,89 @@
+document.getElementById('btn-donate').addEventListener('click',function(){
+
+    
+    const addmoney=getInputFieldValueById('input-donate-amount')
+    const avaliableBalance=getTextFieldValueById('total-donate');
+    const newBalance=avaliableBalance + addmoney;
+    const totalDonate=document.getElementById('total-donate')
+    totalDonate.innerText=newBalance;
+
+    //minus main balance
+    const mainBalance=getTextFieldValueById('main-balance')
+    const totalMainBalance=mainBalance - addmoney;
+   const newTotalMainBalance=document.getElementById('main-balance')
+   newTotalMainBalance.innerText=`${totalMainBalance} BDT`
+    console.log(mainBalance)
+
+
+    
+//     if(addmoney <= 0 || isNaN(addmoney)){
+//         const newBalance=avaliableBalance + addmoney;
+//         const totalDonate=document.getElementById('total-donate')
+//         totalDonate.innerText=newBalance;
+// }
+
+})
+
+
+document.getElementById('btn-donate2').addEventListener('click',function(){
+   console.log('clicked secend btn')
+   const addmoney=getInputFieldValueById('input-donate-amount')
+    const avaliableBalance=getTextFieldValueById('total-donate');
+    const newBalance=avaliableBalance + addmoney;
+    const totalDonate=document.getElementById('total-donate')
+    totalDonate.innerText=newBalance;
+
+    //minus main balance
+    const mainBalance=getTextFieldValueById('main-balance')
+    const totalMainBalance=mainBalance - addmoney;
+   const newTotalMainBalance=document.getElementById('main-balance')
+   newTotalMainBalance.innerText=`${totalMainBalance} BDT`
+    console.log(mainBalance)
+})
+
+//history tab function
+const historyTab = document.getElementById('history-tab')
+historyTab.addEventListener('click',function(){
+    console.log('clicked history tab')
+     historyTab.classList.add(
+        "rounded-xl", "bg-[#B4F461]", "py-2,", "px-6"
+     )
+     const donatioTab =document.getElementById('donation-tab')
+     donatioTab.classList.remove(
+        "rounded-xl", "bg-[#B4F461]", "py-2,", "px-6"
+     )
+     document.getElementById('donate').classList.add('hidden')
+     document.getElementById('history-list').classList.remove('hidden')
+
+     //history item
+     const addmoney=getInputFieldValueById('input-donate-amount')
+     const donatetitle=document.getElementById('donate-title').innerText
+     
+     const historyItem= document.createElement('div')
+        historyItem.className="bg-gray-200 p-3 rounded-2xl mb-3 border-1-2 border-indigo-500";
+        historyItem.innerHTML=`
+        <p class="text-2xl ">${addmoney} Take is ${donatetitle}</p>
+        <p class="text-xl">${new Date().toDateString()}</p>
+        `
+        document.getElementById('history-list').appendChild(historyItem)
+        // const historyContainer=document.getElementById('history-list')
+        // historyContainer.insertBefore(historyItem,historyContainer.firstChild)
+})
+
+//donate tab function
+const donateTab = document.getElementById('donation-tab')
+donateTab.addEventListener('click',function(){
+    donateTab.classList.add(
+        "rounded-xl", "bg-[#B4F461]", "py-2,", "px-6"
+     )
+     const historyTab =document.getElementById('history-tab')
+     historyTab.classList.remove(
+        "rounded-xl", "bg-[#B4F461]", "py-2,", "px-6"
+     )
+     document.getElementById('history-list').classList.add('hidden')
+     document.getElementById('donate').classList.remove('hidden')
+
+
+})
+
+
---
+++ js/test.js
@@ -0,0 +1,13 @@
+//move Home to Blog page
+//  const move=document.getElementById('btn-blog')
+//  move.addEventListener('click',function(){
+//     document.getElementById('donate').classList.add('hidden')
+//     window.location.href = '/blog.html';
+// })
+
+//move Blog to Home page
+document.getElementById('blog-btn').addEventListener('click',function(){
+    window.location.href='/index.html'
+    console.log('clicked home btn');
+    
+})
